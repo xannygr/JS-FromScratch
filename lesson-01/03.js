6907d9e5ca7c34fd83823cd9abaefbd223e9e50e
xannygr <gridiushhko@gmail.com>
2026-05-03 15:30:09 +0300

    03-final2

--- lesson-01/03.js
+++ lesson-01/03.js
@@ -5,18 +5,14 @@
 */
 
 // your code
-let sum = 1;
+let sum = 0;
 let i = 1;
 
 while (i <= 20) {
-  i = i + 1
   
-  if (i % 2 === 0 ) {
-    sum = 0
-  } else {
+  if (i % 2 !== 0 ) {
     sum = sum + i;
-    i = i + 1;
-  }
-  
+  } 
+  i = i + 1;
 }
 console.log (sum)
