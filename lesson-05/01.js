7e4be3a8af6a5589a985722fba7c8e4de56fd201
xannygr <gridiushhko@gmail.com>
2026-05-21 13:50:49 +0300

    05-01

--- lesson-05/01.js
+++ lesson-05/01.js
@@ -15,5 +15,11 @@ const game = {
     gold: 250,
     lumber: 100,
   },
-  addResource() {}
+  addResource(resources, amount) {
+    if (game.resources === false) {
+      console.log("Invalid resource");
+    } else {
+      resources += amount;
+    }
+  }
 }
