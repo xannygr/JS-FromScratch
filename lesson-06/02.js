f0f2e67a2a9ed30a313143381f84bb4b81b1fb3c
xannygr <gridiushhko@gmail.com>
2026-05-25 18:34:28 +0300

    06-02

--- lesson-06/02.js
+++ lesson-06/02.js
@@ -24,4 +24,11 @@ const doubledNumbers = map(numbers, (element, index) => {
 console.log(doubledNumbers) // Должен вывести: [2, 4, 6, 8, 10]
 */
 
-const map = () => {}
+const map = (array, callback) => {
+  const result = [];
+  for (let i = 0; i < array.length; i++){
+    const trans = callback(array[i], i)
+    result.push(trans);
+  }
+  return result
+}
