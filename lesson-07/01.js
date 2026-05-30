16701db2c923a0295e496527ef4e15b15173f27d
xannygr <gridiushhko@gmail.com>
2026-05-30 13:32:11 +0300

    07-01

--- lesson-07/01.js
+++ lesson-07/01.js
@@ -17,3 +17,33 @@ const WEB_TECH_IMAGES = [
   'https://production-it-incubator.s3.eu-central-1.amazonaws.com/file-manager/Image/c8a1f4a6-1337-4899-bdfd-a8c9c7bb806a_css-magic-logo.jpg',
   'https://production-it-incubator.s3.eu-central-1.amazonaws.com/file-manager/Image/784380b9-6937-42a6-bdfe-869835820234_html-magic-logo.jpg',
 ]
+
+let currentIndex = 0;
+
+const img = document.getElementById('web-tech-image')
+
+const prevButton = document.getElementById('prev-button')
+const nextButton = document.getElementById('next-button')
+
+prevButton.addEventListener('click', function(){
+  if (currentIndex === 0){
+    currentIndex = WEB_TECH_IMAGES.length - 1
+  } else {
+    currentIndex--
+  }
+  img.src = WEB_TECH_IMAGES[currentIndex]
+  // img.document
+  // img.remove() 
+  // for (let i = 0; i < WEB_TECH_IMAGES.length, i++){
+  
+  // }
+})
+
+nextButton.addEventListener('click', function(){
+  if (currentIndex === WEB_TECH_IMAGES.length - 1){
+    currentIndex = 0
+  } else {
+    currentIndex++
+  }
+  img.src = WEB_TECH_IMAGES[currentIndex]
+})
